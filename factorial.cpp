0a6304297b66223134392cb849a681e9bc8d4f38
charishmaa-2007 <acharishma2007@gmail.com>
2026-06-30 20:29:22 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long factorial(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << factorial(n);
+
+    return 0;
+}  
