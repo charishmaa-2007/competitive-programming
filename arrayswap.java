af959de644de360f29ef80492bbc8f0ca31a57da
charishmaa-2007 <acharishma2007@gmail.com>
2026-06-30 20:33:24 +0530

    Add files via upload

---
+++ arrayswap.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class arrayswap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        int maxIndex = 0;
+        int minIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
+
+            if (arr[i] < arr[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+                int temp = arr[maxIndex];
+        arr[maxIndex] = arr[minIndex];
+        arr[minIndex] = temp;
+
+        // Print the modified array
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
