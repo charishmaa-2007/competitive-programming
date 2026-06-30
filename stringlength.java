7dcd390f8a43dd86a233ad9ed58345a639bad3ea
charishmaa-2007 <acharishma2007@gmail.com>
2026-06-30 20:31:32 +0530

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class stringlength {
+
+    public static void main(String[] args) {
+        
+    
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.print(str.length());
+
+        sc.close();
+    }
+}    
